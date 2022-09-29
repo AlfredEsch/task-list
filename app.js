7023456721479d46959460b47c1092e5195ce309
AlfredEsch <alfred.eschbaum@khk.ee>
2022-09-29 11:10:46 +0300

    by clicking on the x the user can delete a task from the list [Delivers #183413657]

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const form = document.querySelector('form')
 const taskInput = document.querySelector('#task')
+const taskList = document.querySelector('ul')
 
 form.addEventListener('submit', addTask)
+taskList.addEventListener('click',deleteTask)
 
-function addTask(e){
-    const li = document.createElement('li')
-    li.appendChild(document.createTextNode(taskInput.value))
-    li.className = 'collection-item'
-    const a = document.createElement('a')
-    a.appendChild(document.createElement('x'))
-    a.className = 'blue-text text-darken2 secondary-content'
-    a.setAttribute('href', '#')
-    li.appendChild(a)
-    // add to list
-    const ul = document.querySelector('ul')
-    ul.appendChild(li)
-    taskInput.value = " "
-    e.preventDefault()
+function addTask(e){}
+
+function deleteTask(e){
+    if (e.target.textContent == 'X'){
+        if(confirm('Are you sure to delete this task')){
+        } e.target.parentElement.remove()
+    }
 }
