1826dc777a345d1307a30675bd7b3b518ef5c1f9
AlfredEsch <alfred.eschbaum@khk.ee>
2022-10-03 12:55:59 +0300

    by clicking on the x the user can delete a task from the list [Delivers #183413944]

--- app.js
+++ app.js
@@ -6,34 +6,31 @@ const deleteallTasks = document.querySelector('#del-tasks')
 form.addEventListener('submit', addTask)
 taskList.addEventListener('click',deleteTask)
 deleteallTasks.addEventListener('click', deleteallTasks)
-
 function addTask(e){
-    // create list item
     const li = document.createElement('li')
     li.appendChild(document.createTextNode(taskInput.value))
     li.className = 'collection-item'
     const a = document.createElement('a')
-    a.appendChild(document.createTextNode('X'))
-    a.className = 'blue-text text-darken-2 secondary-content'
+    a.appendChild(document.createElement('x'))
+    a.className = 'blue-text text-darken2 secondary-content'
     a.setAttribute('href', '#')
     li.appendChild(a)
     // add to list
     const ul = document.querySelector('ul')
     ul.appendChild(li)
-    taskInput.value = ''
+    taskInput.value = " "
     e.preventDefault()
 }
 
-function deleteTask(e){
-    if(e.target.textContent == 'X'){
-        if(confirm('Are you sure to delete this task?')){
-            e.target.parentElement.remove()
-        }
+function deleteTask(e){}
+if(e.target.textContent == 'X'){
+    if(confirm('Are you sure to delete this task?')){
+        e.target.parentElement.remove()
     }
 }
 
-function deleteAllTasks(e){
-    //taskList.innerHTML = ''
+
+function deleteallTasks(e){
     while (taskList.firstChild){
         taskList.removeChild(taskList.firstChild)
     }
