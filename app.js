6e7cc762274dfadea7a78a308db722ded5495d55
AlfredEsch <alfred.eschbaum@khk.ee>
2022-10-11 15:08:55 +0300

    by clicking on the x the user can delete a task from the list [Delivers #183463120]

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const form  = document.querySelector('form')
 const taskInput = document.querySelector('#task')
 const taskList = document.querySelector('ul')
 const delAllTasks = document.querySelector('#del-tasks')
+const searchInput = document.querySelector('tasklist')
 
 form.addEventListener('submit', addTask)
 taskList.addEventListener('click', deleteTask)
 delAllTasks.addEventListener('click', deleteAllTasks)
+document.addEventListener('DOMContentLoaded', getTasks)
 
 function addTask(e){
     // create list item
@@ -20,6 +22,7 @@ function addTask(e){
     // add to list
     const ul = document.querySelector('ul')
     ul.appendChild(li)
+    addTaskLS(taskInput.value)
     taskInput.value = ''
     e.preventDefault()
 }
@@ -34,14 +37,16 @@ function deleteTask(e){
 }
 
 function deleteAllTasks(e){
+    //taskList.innerHTML = ''
     while (taskList.firstChild){
         taskList.removeChild(taskList.firstChild)
     }
     localStorage.removeItem('tasks')
 }
-function addTaskLS(task){
+
+function addTaskLS(task) {
     let tasks
-    if(localStorage.getItem('tasks') == null ){
+    if(localStorage.getItem('tasks') === null){
         tasks = []
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'))
@@ -50,16 +55,16 @@ function addTaskLS(task){
     localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
-function deleteTaskLS(task){
+function deleteTaskLS(task) {
     let tasks
-    if(localStorage.getItem('tasks') == null ){
+    if(localStorage.getItem('tasks') === null){
         tasks = []
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'))
     }
-    task.forEach((taskLS, taskIndex ) => {
-        if(taskLS == task){
-            task.splice(taskIndex, 1)
+    tasks.forEach((taskLS, taskIndex) => {
+        if(taskLS === task){
+            tasks.splice(taskIndex, 1)
         }
     })
     localStorage.setItem('tasks', JSON.stringify(tasks))
@@ -76,6 +81,10 @@ function getTasks(){
         const li = document.createElement('li')
         li.appendChild(document.createTextNode(task))
         li.className = 'collection-item'
+        const b = document.createElement("b")
+        b.appendChild(document.createTextNode(task))
+        b.style.display = "none"
+        li.appendChild(b)
         const a = document.createElement('a')
         a.appendChild(document.createTextNode('X'))
         a.className = 'blue-text text-darken-2 secondary-content'
@@ -85,4 +94,22 @@ function getTasks(){
         const ul = document.querySelector('ul')
         ul.appendChild(li)
     })
+}
+
+function filter(e){
+    var input, filter1, ul, li, a, i, txtvalue;
+    input = document.getElementById('task1')
+    filter1 = input.value.toUpperCase();
+    ul = document.getElementById('minu');
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName('b')[0];
+        txtvalue = a.textContent || a.innerText;
+        if (txtvalue.toUpperCase().indexOf(filter1) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
